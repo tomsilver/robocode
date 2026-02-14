b358714bb426343f9676ac06b86664613c2b7420
Tom Silver <tomssilver@gmail.com>
2026-02-14 15:51:12 -0500

    wip

--- src/robocode/simulators/base_simulator.py
+++ src/robocode/simulators/base_simulator.py
@@ -4,6 +4,7 @@ import abc
 from typing import Generic, TypeVar
 
 import numpy as np
+from gymnasium.spaces import Space
 
 _StateType = TypeVar("_StateType")
 _ActType = TypeVar("_ActType")
@@ -11,6 +12,16 @@ _ActType = TypeVar("_ActType")
 
 class BaseSimulator(Generic[_StateType, _ActType], abc.ABC):
     """A simulator exposes only a sample_next_state function."""
+
+    @property
+    @abc.abstractmethod
+    def action_space(self) -> Space[_ActType]:
+        """The action space of the simulator."""
+
+    @property
+    @abc.abstractmethod
+    def observation_space(self) -> Space[_StateType]:
+        """The observation space of the simulator."""
 
     @abc.abstractmethod
     def sample_next_state(
