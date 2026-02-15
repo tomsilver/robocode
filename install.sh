d79032d1f94dfe94971122e121f008796a50a598
Tom Silver <tomssilver@gmail.com>
2026-02-15 10:44:43 -0500

    wip

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -e
 git submodule update --init --recursive
-uv pip install \
-    -e prpl-mono/relational-structs \
-    -e prpl-mono/prpl-utils \
-    -e prpl-mono/toms-geoms-2d \
-    -e prpl-mono/kinder \
-    -e ".[develop]"
+uv sync --all-extras --dev
