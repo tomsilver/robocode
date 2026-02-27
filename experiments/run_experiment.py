068b8c366baccca8712d7daeb3fff1b882f01651
yichao-liang <ycliang6@gmail.com>
2026-02-27 16:43:07 +0000

    format

--- experiments/run_experiment.py
+++ experiments/run_experiment.py
@@ -13,7 +13,7 @@ Parallel sweep with joblib launcher:
         approach.use_docker=true \
         seed=42,24,424,444,222 \
         'primitives=[]' \
-        environment=motion2d_easy,obstruction2d_easy,clutteredretrieval2d_easy,clutteredstorage2d_easy,stickbutton2d_easy,pushpullhook2d \
+        environment=motion2d_easy,obstruction2d_easy,clutteredretrieval2d_easy \
         'hydra.sweep.dir=multirun/2026-02-23/no_primitives_5d_s42_24_424_444_222' \
         'hydra.sweep.subdir=s${seed}/${hydra:runtime.choices.environment}' \
         hydra/launcher=joblib hydra.launcher.n_jobs=4
