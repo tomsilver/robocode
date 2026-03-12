0d5c6d23478e593994b3c0b90c71161077445329
Bowen Li <bowenli2@andrew.cmu.edu>
2026-03-12 11:25:06 -0400

    start implementing the multi session gradual improve approach

--- scripts/run_all2d_no_primitives_25d_b.sh
+++ scripts/run_all2d_no_primitives_25d_b.sh
@@ -7,6 +7,6 @@ python experiments/run_experiment.py -m \
     approach.max_budget_usd=25 \
     seed=424 \
     'primitives=[]' \
-    environment=clutteredretrieval2d_medium,clutteredretrieval2d_hard,clutteredstorage2d_medium,clutteredstorage2d_hard,stickbutton2d_medium,stickbutton2d_hard \
-    'hydra.sweep.dir=multirun/2026-02-25/no_primitives_25d_all2d_s424' \
+    environment=clutteredstorage2d_medium,stickbutton2d_medium,obstruction2d_medium,clutteredretrieval2d_medium \
+    'hydra.sweep.dir=multirun/2026-03-10/no_primitives_25d_all2d_s424' \
     'hydra.sweep.subdir=s${seed}/${hydra:runtime.choices.environment}'
