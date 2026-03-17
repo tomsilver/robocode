8a698c21b51de3fc51194977e5a9a07cb5b53fd3
Bowen Li <bowenli2@andrew.cmu.edu>
2026-03-17 14:23:52 -0400

    enabled mcp tests

--- run_ci_checks.sh
+++ run_ci_checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 ./run_autoformat.sh
-mypy . --exclude prpl-mono --exclude outputs --exclude multirun --exclude 'src/robocode/mcp' --exclude 'tests/mcp'
-pytest . --pylint -m pylint --pylint-rcfile=.pylintrc --ignore=prpl-mono --ignore=outputs --ignore=multirun --ignore=tests/mcp --ignore=src/robocode/mcp
+mypy . --exclude prpl-mono --exclude outputs --exclude multirun --exclude 'src/robocode/mcp'
+pytest . --pylint -m pylint --pylint-rcfile=.pylintrc --ignore=prpl-mono --ignore=outputs --ignore=multirun
 pytest tests/ --ignore=tests/mcp
