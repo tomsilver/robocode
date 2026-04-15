63847e980a4ca5e5650acbe69b6ceed906ed6efb
merlerm <matteo.merler@gmail.com>
2026-04-15 14:19:01 +0200

    exclude analysis from ci cd

--- run_autoformat.sh
+++ run_autoformat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python -m black . --exclude 'prpl-mono|\.venv|outputs|multirun'
-docformatter -i -r . --exclude venv .venv prpl-mono outputs multirun
-isort . --skip .venv --skip prpl-mono --skip outputs --skip multirun
+python -m black . --exclude 'prpl-mono|\.venv|outputs|multirun|analysis'
+docformatter -i -r . --exclude venv .venv prpl-mono outputs multirun analysis
+isort . --skip .venv --skip prpl-mono --skip outputs --skip multirun --skip analysis
--- run_ci_checks.sh
+++ run_ci_checks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 ./run_autoformat.sh
-mypy . --exclude prpl-mono --exclude outputs --exclude multirun --exclude 'src/robocode/mcp'
-pytest . --pylint -m pylint --pylint-rcfile=.pylintrc --ignore=prpl-mono --ignore=outputs --ignore=multirun
+mypy . --exclude prpl-mono --exclude outputs --exclude multirun --exclude analysis --exclude 'src/robocode/mcp'
+pytest . --pylint -m pylint --pylint-rcfile=.pylintrc --ignore=prpl-mono --ignore=outputs --ignore=multirun --ignore=analysis
 pytest tests/ --ignore=tests/mcp
